1e86908606e83eb00897a7d76207dac6cfb12253
OnlineGBC <OnlineGBC@users.noreply.github.com>
2026-03-27 14:42:44 -0400

    Load .env file via python-dotenv in app.py

--- app/app.py
+++ app/app.py
@@ -1,6 +1,9 @@
 import os
 import logging
+from dotenv import load_dotenv
 from flask import Flask, render_template, jsonify
+
+load_dotenv()
 
 # ---------------------------------------------------------------------------
 # Logging
